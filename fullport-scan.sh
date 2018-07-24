82f7e277223ae31638fc85ddc5f09cec129fec26
Michael Rocha <33291574+Essex09@users.noreply.github.com>
2018-07-24 08:36:54 -0400

    Update fullport-scan.sh

--- fullport-scan.sh
+++ fullport-scan.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 # Takes user input to scan a host, creates directory, subfolders titled exploit and scans, then creates a txt file called notes.
+
 ###################
 # Add color
 ###################
+
 red='\033[0;31m'
 nc='\033[0m' # No Color
 
-if [ $# -ne 2 ]; then
-    echo -e "${red}Usage: $0 [IP Address] [Directory] (e.g., /home)${nc}"
+if [ $# -ne 3 ]; then
+    echo -e "${red}Usage: $0 [IP Address] [Directory] (e.g., /home) [quick/full]${nc}"
     exit 1
 fi
 
 ###################
 # Create directories
 ###################
+
 newdir=$2/$1
 if [ -d $newdir ] # Check if directory already exists
     then
@@ -26,22 +29,30 @@ sleep 1;
 
 ###################
 # Full TCP scan with Nmap
+# Nmap commands from @killswitch-GUI
 ###################
-echo -e "${red}Beginning Nmap scans on $1${nc}"
-sleep .5;
 
-###################
-# From @killswitch-GUI
-# Full port scan
-###################
-nmap -Pn -sS -p- -sV --min-hostgroup 255 --min-rtt-timeout 25ms --max-rtt-timeout 100ms --max-retries 1 --max-scan-delay 0 --min-rate 1000 -oA $newdir/scans/fullport -vvv --open $
-sleep 1;
+if [[ $3 = quick ]]; then
+    echo -e "${red}Beginning quick Nmap scans on $1${nc}"
+    nmap -Pn -sS --min-hostgroup 255 --min-rtt-timeout 25ms --max-rtt-timeout 100ms --max-retries 1 --max-scan-delay 0 --min-rate 2000 -oA $newdir/scans/quickscan -vvv --open $1	
+elif [[ $3 = full ]]; then
+    echo -e "${red}Beginning fullport Nmap scan on $1${nc}"
+    nmap -Pn -sS -p- -sV --min-hostgroup 255 --min-rtt-timeout 25ms --max-rtt-timeout 100ms --max-retries 1 --max-scan-delay 0 --min-rate 1000 -oA $newdir/scans/fullport -vvv --open $1
+
+else
+   echo -e "${red}You didn't select a scan option of quick or full!${nc}"
+fi
+    
+sleep .5;
 echo -e "\n"
 echo -e "${red}Findings written to $newdir/scans/${nc}"
+
+sleep .5;
+echo -e "\n"
+echo -e "${red}Goodbye!${nc}"
 exit
 
 ###################
 # Coming soon:
 # Write predefined commands to run based on detected services
-# Options to choose between quick and full port scan
 ###################
