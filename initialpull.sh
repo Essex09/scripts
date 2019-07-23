a955d71e8b8f2743911a97c6fa8719ffe3c3e697
Michael Rocha <33291574+Essex09@users.noreply.github.com>
2019-07-23 06:44:54 -0700

    Update initialpull.sh

--- initialpull.sh
+++ initialpull.sh
@@ -4,14 +4,21 @@ dir=/opt/scripts
 
 if [ -d $dir ] # Check if directory already exists
     then
-    echo -e "$dir directory already exists"
+    echo -e "#################################"
+    echo -e "# $dir directory already exists #"
+    echo -e "#################################"
 else
-    echo -e "Making $dir directory"
+    echo -e "#########################"
+    echo -e "# Making $dir directory #"
+    echo -e "#########################"
     mkdir -p $dir && cd $dir
 fi
 sleep 1;
 
 # Clone repos
+echo -e "#################"
+echo -e "# Cloning Repos #"
+echo -e "#################"
 git clone https://github.com/harleyQu1nn/AggressorScripts.git
 git clone https://github.com/Essex09/Kali-i3gaps-Install.git
 git clone https://github.com/Essex09/dotfiles.git
